24377bbe538e48e2d9390736e39d1e83a06f1b52
shivam2144 <shivamdalve123@gmail.com>
2025-07-12 01:31:36 +0530

    Backend chage

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ dotenv.config({});
 
 const app = express();
 
+// Allowed origins list
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://job-portal-frontend-neon.vercel.app/"
+];
+
+// CORS Middleware
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
+    next();
+});
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
